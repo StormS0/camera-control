096272326246b3be7ff44345f3486b3c0db00611
strangerintheq <tripmannn@gmail.com>
2017-05-21 12:20:18 +0300

    ui

--- html/scripts/main.js
+++ html/scripts/main.js
@@ -1,18 +1,5 @@
 
 var debugMode = window.location.search.indexOf('debug') > 0;
-var pollingInterval = debugMode ? 10000: 1000;
-
-window.applyClassForSelectors = function (selectors, className, action) {
-    selectors.forEach(function (selector) {
-        forEachSelector(selector, function (node) {
-            node.classList[action](className);
-        });
-    });
-};
-
-window.forEachSelector = function (selector, func) {
-    Array.prototype.forEach.call(document.querySelectorAll(selector), func);
-};
 
 (function() {
     var connections = [
@@ -28,7 +15,7 @@ window.forEachSelector = function (selector, func) {
     document.querySelector("#button_settings").onclick = toggleHidden.bind(null, ['.control_cameras']);
 
     forEachSelector('.camerabox', initCamerabox);
-    setInterval(update, pollingInterval);
+    setInterval(update, debugMode ? 10000: 1000);
 
     connections.forEach(function (conn) {
         if (conn.enabled) conn.reconnect();
@@ -39,42 +26,48 @@ window.forEachSelector = function (selector, func) {
         canonVideo.classList.toggle('full_screen');
     }, false);
 
+
+
     function initCamerabox(box) {
         var settingsButton = box.querySelector('.camerabox_title__right-settings');
+        if (!settingsButton) return;
         settingsButton.title = 'Настройки';
         settingsButton.onclick = function() {
-
-            var camerasPageElements = [];
-            connections.forEach(function (connection) {
-                if (connection.enabled) {
-                    camerasPageElements.push('#camerabox_' + connection.id);
-                }
-            });
-
-            var settingsPageElements = ['.camerabox-stub', '.settings_page', '#' + box.id];
-
             var settings = document.querySelector('.settings_page');
             var hidden = settings.classList.contains('hidden');
+            var settingsPageElements = ['.camerabox-stub', '#buttons_mask', '.settings_page', '#' + box.id];
+            var camerasPageElements = [];
+            connections.forEach(function (connection) {
+                if (connection.enabled) camerasPageElements.push('#camerabox_' + connection.id);
+            });
+
             hideAll(hidden ? camerasPageElements : settingsPageElements);
             showAll(hidden ? settingsPageElements : camerasPageElements);
 
-            var iframe = settings.querySelector('iframe');
-            if (hidden) {
-                iframe.onload = function() {
-                    setTimeout(box.connection.settingsPageLoaded, 1000);
-                };
-                iframe.src = box.settingsPageUrl;
-                settingsButton.classList.add('activated');
-                settingsButton.title = 'Закрыть настройки';
-                box.connection.settingsOpened(box);
-            } else {
-                iframe.onload = function(){};
-                iframe.src = 'settings.html';
-                settingsButton.classList.remove('activated');
-                settingsButton.title = 'Настройки';
-                box.connection.settingsClosed(box);
-            }
+            document.querySelector('.control_cameras').classList.add('hidden');
+
+            setupIframe(hidden, box, settingsButton);
         };
+    }
+
+    function setupIframe(hidden, box, settingsButton) {
+        var iframe = document.querySelector('iframe');
+        if (hidden) {
+            iframe.onload = function () {
+                setTimeout(box.connection.settingsPageLoaded, 1000);
+            };
+            iframe.src = box.settingsPageUrl;
+            settingsButton.classList.add('activated');
+            settingsButton.title = 'Закрыть настройки';
+            box.connection.settingsOpened(box);
+        } else {
+            iframe.onload = function () {
+            };
+            iframe.src = 'settings.html';
+            settingsButton.classList.remove('activated');
+            settingsButton.title = 'Настройки';
+            box.connection.settingsClosed(box);
+        }
     }
 
     function update() {
@@ -101,6 +94,18 @@ window.forEachSelector = function (selector, func) {
 
     function showAll(selectors) {
         applyClassForSelectors(selectors, 'hidden', 'remove')
+    }
+
+    function applyClassForSelectors(selectors, className, action) {
+        selectors.forEach(function (selector) {
+            forEachSelector(selector, function (node) {
+                node.classList[action](className);
+            });
+        });
+    }
+
+    function forEachSelector(selector, func) {
+        Array.prototype.forEach.call(document.querySelectorAll(selector), func);
     }
 
 })();
